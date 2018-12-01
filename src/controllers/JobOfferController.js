1ada1918efc85fa2ed71334c5858143439209d57
lukhol <lukasz.holdrowicz@gmail.com>
2018-12-01 17:47:30 +0100

    Moved controllers to class syntax.

--- src/controllers/JobOfferController.js
+++ src/controllers/JobOfferController.js
@@ -5,110 +5,114 @@ const moment = require('moment');
 const Joi = require('joi');
 const jobOfferValidator = require('../validation/JobOfferValidator');
 
-exports.getAll = (req, res, next) => {
-    JobOffer.find((err, docs) => {
-        if(err) {
-            res.status(500);
-            return res.json({
-                message: "Something went wrong. Sorry :("
-            });
-        }
-
-        res.status(200);
-        res.json(docs.map(item => {
-            return {
-                id: item._id,
-                title: item.title,
-                from: item.from,
-                companyName: item.companyName,
-                categoryId: item.category._id
-            }
-        }));
-    });
-};
-
-exports.getById = (req, res, next) => {
-    JobOffer.findOne({_id: req.params.id}, function(err, doc) {
-        if(err || !doc) {
-            res.status(404);
-            return res.json({});
-        }
-
-        res.status(200);
-        res.json({
-            id: doc._id,
-            title: doc.title,
-            from: doc.from,
-            to: doc.to, 
-            companyName: doc.companyName,
-            categoryId: doc.category._id
-        });
-    });
-};
-
-exports.create = (req, res, next) => {
-    const validationResult = Joi.validate(req.body, jobOfferValidator);
-    if(validationResult.error) {
-        res.status(400);
-        return res.json(validationResult.error.details.map(d => d.message));
-    }
-
-    Category.findOne({_id: req.body.categoryId}, function(err, doc) {
-        if(err) {
-            res.status(500);
-            return res.json({
-                message: "CategoryId cannot be empty."
-            });
-        }
-
-        const jobOffer = new JobOffer({
-            _id: new mongoose.Types.ObjectId(),
-            title: req.body.title,
-            from: moment(req.body.from, 'DD-MM-YYYY').toDate(),
-            to: moment(req.body.to, 'DD-MM-YYYY').toDate(),
-            companyName: req.body.companyName,
-            category: doc._id,
-            user: req.userData.id
-        })
-        .save(function(err, result) {
+class JobOfferController {
+    getAll (req, res, next) {
+        JobOffer.find((err, docs) => {
             if(err) {
                 res.status(500);
                 return res.json({
-                    message: "Saving failed. Try again later."
+                    message: "Something went wrong. Sorry :("
                 });
             }
-
-            res.status(201);
-            res.location(result.getLocation());
-            res.end();
-        });;
-    });
-};
-
-exports.search = async (req, res, next) => {
-    try {
-        const result = await JobOffer.find({
-            $and: [
-                { category: { $in : req.query.categoryId } },
-                { user: { $in: req.query.userId } }
-            ]
-        });
-        
-        res.status(200);
-        res.json(result.map(it => {
-            return {
-                id: it._id,
-                from: it.from,
-                to: it.to,
-                companyName: it.companyName,
-                category: it.category,
-                user: it.user
-            }
-        }));
-    } catch (e) {
-        res.status(400);
-        res.json({
-            message: "Database call failed."
+    
+            res.status(200);
+            res.json(docs.map(item => {
+                return {
+                    id: item._id,
+                    title: item.title,
+                    from: item.from,
+                    companyName: item.companyName,
+                    categoryId: item.category._id
+                }
+            }));
         });
     }
+
+    getById(req, res, next) {
+        JobOffer.findOne({_id: req.params.id}, function(err, doc) {
+            if(err || !doc) {
+                res.status(404);
+                return res.json({});
+            }
+    
+            res.status(200);
+            res.json({
+                id: doc._id,
+                title: doc.title,
+                from: doc.from,
+                to: doc.to, 
+                companyName: doc.companyName,
+                categoryId: doc.category._id
+            });
+        });
+    }
+
+    create(req, res, next) {
+        const validationResult = Joi.validate(req.body, jobOfferValidator);
+        if(validationResult.error) {
+            res.status(400);
+            return res.json(validationResult.error.details.map(d => d.message));
+        }
+    
+        Category.findOne({_id: req.body.categoryId}, function(err, doc) {
+            if(err) {
+                res.status(500);
+                return res.json({
+                    message: "CategoryId cannot be empty."
+                });
+            }
+    
+            const jobOffer = new JobOffer({
+                _id: new mongoose.Types.ObjectId(),
+                title: req.body.title,
+                from: moment(req.body.from, 'DD-MM-YYYY').toDate(),
+                to: moment(req.body.to, 'DD-MM-YYYY').toDate(),
+                companyName: req.body.companyName,
+                category: doc._id,
+                user: req.userData.id
+            })
+            .save(function(err, result) {
+                if(err) {
+                    res.status(500);
+                    return res.json({
+                        message: "Saving failed. Try again later."
+                    });
+                }
+    
+                res.status(201);
+                res.location(result.getLocation());
+                res.end();
+            });;
+        });
+    }
+
+    async search(req, res, next) {
+        try {
+            const result = await JobOffer.find({
+                $and: [
+                    { category: { $in : req.query.categoryId } },
+                    { user: { $in: req.query.userId } }
+                ]
+            });
+            
+            res.status(200);
+            res.json(result.map(it => {
+                return {
+                    id: it._id,
+                    from: it.from,
+                    to: it.to,
+                    companyName: it.companyName,
+                    category: it.category,
+                    user: it.user
+                }
+            }));
+        } catch (e) {
+            res.status(400);
+            res.json({
+                message: "Database call failed."
+            });
+        }
+    };
 };
+
+module.exports = new JobOfferController();
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,100 +3,104 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.register = (req, res, next) => {
-    User.findOne({email: req.body.email}).exec()
-        .then(user => {
-            if(user != null) {
-                res.status(409).json({message: "Invalid email. Choose another one."});
-                return;
-            }
-
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if(err) {
-                    res.status(409).json({message: "Something went wrong"});
-                } else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash,
-                        active: true,
-                        roles: [
-                            'ROLE_USER'
-                        ]
-                    });
-
-                    user.save().then(doc => {
-                        res.status(201).json({message: "User created"});
-                    })
-                    .catch(err => {
-                        res.status(400).json({message: "Error ocurred during creating user"});
-                    });
+class UsersController {
+    register(req, res, next) {
+        User.findOne({email: req.body.email}).exec()
+            .then(user => {
+                if(user != null) {
+                    res.status(409).json({message: "Invalid email. Choose another one."});
+                    return;
                 }
-            });
-        }) 
-        .catch(err => {
-            res.status(400).json({message: "This email is occupied."});
-        })
-};
-
-exports.login = (req, res, next) => {
-    User.find({email: req.body.email}, (err, user) => {
-        if(user.length < 1) {
-            res.status(401);
-            res.json({message: "Auth failed."}); 
-            return;
-        }
-
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            if(err || !result) {
+    
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        res.status(409).json({message: "Something went wrong"});
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                            active: true,
+                            roles: [
+                                'ROLE_USER'
+                            ]
+                        });
+    
+                        user.save().then(doc => {
+                            res.status(201).json({message: "User created"});
+                        })
+                        .catch(err => {
+                            res.status(400).json({message: "Error ocurred during creating user"});
+                        });
+                    }
+                });
+            }) 
+            .catch(err => {
+                res.status(400).json({message: "This email is occupied."});
+            })
+    }
+    
+    login(req, res, next) {
+        User.find({email: req.body.email}, (err, user) => {
+            if(user.length < 1) {
                 res.status(401);
-                res.json({message: "Authorization failed."}); 
+                res.json({message: "Auth failed."}); 
                 return;
             }
-
-            const token = jwt.sign({
-                id: user[0]._id,
-                email: user[0].email,
-                roles: user[0].roles
-            }, 'secret-key', {
-                expiresIn: "24h"
+    
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err || !result) {
+                    res.status(401);
+                    res.json({message: "Authorization failed."}); 
+                    return;
+                }
+    
+                const token = jwt.sign({
+                    id: user[0]._id,
+                    email: user[0].email,
+                    roles: user[0].roles
+                }, 'secret-key', {
+                    expiresIn: "24h"
+                });
+                
+                res.setHeader('Authorization', `Bearer ${token}`);
+                res.status(200);
+                res.json({
+                    token: token
+                });
             });
-            
-            res.setHeader('Authorization', `Bearer ${token}`);
-            res.status(200);
-            res.json({
-                token: token
-            });
-        });
-    });
-};
-
-exports.delete = (req, res, next) => {
-    if(req.userData.id === req.params.id) {
-        User.remove({_id: req.params.id}, (err, doc) => {
-            res.status(204);
-            res.json({
-                message: "Deleted successfully."
-            });
-        });
-    } else {
-        res.status(400);
-        res.json({
-            message: "This delete operation is not allowed."
         });
     }
-};
-
-exports.getById = (req, res, next) => {
-    User.findOne({_id: req.params.id}, function(err, doc) {
-        if(err) {
-            res.status(404);
+    
+    delete(req, res, next) {
+        if(req.userData.id === req.params.id) {
+            User.remove({_id: req.params.id}, (err, doc) => {
+                res.status(204);
+                res.json({
+                    message: "Deleted successfully."
+                });
+            });
+        } else {
+            res.status(400);
             res.json({
-                message: err
+                message: "This delete operation is not allowed."
             });
         }
+    }
+    
+    getById(req, res, next) {
+        User.findOne({_id: req.params.id}, function(err, doc) {
+            if(err) {
+                res.status(404);
+                res.json({
+                    message: err
+                });
+            }
+    
+            res.status(200);
+            res.json(doc);
+        });
+    }
+}
 
-        res.status(200);
-        res.json(doc);
-    });
-};
+module.exports = new UsersController();
